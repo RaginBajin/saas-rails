b854be944cbca7ef25429f425ac0fb0cad0c9b2c
Joseph Bajin <josephbajin@gmail.com>
2013-09-03 22:02:56 -0400

    Devise Email

---
+++ config/initializers/devise.rb
@@ -0,0 +1,4 @@
+# ==> Mailer Configuration
+# # Configure the e-mail address which will be shown in Devise::Mailer,
+# # note that it will be overwritten if you use your own mailer class with default "from" parameter.
+config.mailer_sender = ENV["EMAIL_ADDRESS"]
